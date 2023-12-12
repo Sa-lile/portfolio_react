21656e0fb3660b282d624d19309730de65a7495a
Sachiyo <sachiyo.sable@gmail.com>
2023-12-12 12:31:44 +0100

    update services

--- src/constants/data.js
+++ src/constants/data.js
@@ -5,25 +5,25 @@ const services = [
         id:1,
         image: `${images.services_img_1}`,
         title: "À props de moi",
-        paragraph: "Je suis Japonaise. J'ai plusieurs années d'expérience en tant qu'hôtesse d'accueil et réceptionniste bilingue dans un hôtel et sur des croisières. J'ai rencontré de nombreuses personnes de nationalités différentes au cours de ces dernières années. J'aime aider, assister et travailler en équipe.",
+        paragraph: "Japonaise avec une expérience diversifiée, j'ai développé des compétences clés, notamment en assistance, résolution de problèmes, et apprentissage continu. Travaillant dans une start-up au sein d'une école anglaise et participant à l'inauguration de deux bateaux de croisière, j'ai acquis une compréhension approfondie du travail d'équipe et de la contribution au développement d'une entreprise. Ma capacité d'adaptation, créativité, et souci du détail sont des atouts que je peux offrir.",
     },
     {
         id:2,
         image: `${images.services_img_3}`,
-        title: "Education",
-        paragraph: "En tant qu'hôtesse d'accueil bilingue, j'ai acquis une expérience solide dans l'assistance aux personnes et la résolution de problèmes. Mon parcours comprend une expérience en start-up dans une école anglaise au Japon, ainsi que la participation à l'inauguration de deux bateaux de croisière, renforçant ma capacité à contribuer au travail d'équipe et au développement d'une entreprise. ",
+        title: "Formation",
+        paragraph: "J'ai obtenu un diplôme en gestion hôtelière. En 2021, j'ai suivi une formation de projet professionnel et réalisé un stage de cinq semaines en Web design. En 2022, j'ai obtenu mon diplôme de Développeur web et web mobile (niveau bac+2) après une formation chez LDNR. Actuellement, en 2023, je suis en contrat d'alternance chez Prooftag et en voie d'obtenir mon diplôme de Concepteur développeur d'applications (niveau bac+3/4).",
     },
     {
         id:3,
         image: `${images.services_img_2}`,
         title: "Compétences",
-        paragraph:  " En 2022, Formation chez LDNR : Diplomé Développeur web / web mobile(niveau bac+2). J'ai appris HTML/CSS, JS, C++, JAVA, Angular MySQL, CMS, Dans le stage with Python, Django et BootStrap. En 2023, Contrat d'alternance chez Prooftag: Diplomé Concepteur développeur d'applications (niveau bac+3/4). J'ai appris Sécurité, AI, ",
+        paragraph:  " Au fil de mes formations et stages, j'ai développé des compétences variées, allant de CMS, HTML/CSS, JS, C, C++, JAVA, Angular, Android à MySQL. En 2022, lors d'un stage de deux mois, j'ai concrétisé le projet Graine de fil avec Python, Django et Bootstrap. J'ai ensuite approfondi mes connaissances en Java, bases de données, UML, Design pattern, Python, Django, IA, sécurité. En début d'année, j'ai participé à un projet de site de vin QRNUTRI utilisant React, MUI, Sass, Node.js et TypeScript." 
     },
     {
         id:4,
         image: `${images.services_img_4}`,
-        title: "Loisirs",
-        paragraph:  " J'aime bien faire de la création (DIY), de la décoration, et j'aime prendre des photos lors de mes voyages. Quand j'étais au lycée, j'ai pratiqué le bâton twirling. Actuellement, je fais du yoga et des étirements tous les matins.",
+        title: "Qualités",
+        paragraph:  "Je suis une personne créative passionnée par la décoration. Ma rigueur et mon organisation se manifestent à travers la définition d'objectifs hebdomadaires et annuels. Ma motivation est forte, alimentée par le désir constant d'améliorer mes compétences.",
     },
 ];
 

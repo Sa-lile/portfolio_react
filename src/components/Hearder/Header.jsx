159dca6e433e8138cda46968bc0aa712b2d16502
Sachiyo <sachiyo.sable@gmail.com>
2024-01-18 18:36:05 +0100

    Modify mon profile comment

--- src/components/Hearder/Header.jsx
+++ src/components/Hearder/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
             <h1 className='text-uppercase text-white op-07 fw-6 ls-2'>Sachiyo SABLÉ</h1>
             <h3 className='header-title text-white fw-6 '>BONJOUR, JE SUIS</h3>
             <h3 className='header-title text-white fw-6 '> <span className='text-orange'> Développeur Web Full Stack</span></h3>
-            <h3 className='text-grey fw-6'>Je souhaite continuer à améliorer mes compétences en utilisant ce que j'ai appris jusqu'à présent. Je suis en train d'étudier de manière autonome avec Python et React, ainsi que les programmes de cybersécurité et d'UX de Google.</h3>
+            <h3 className='text-grey fw-6'>En quête d'amélioration continue, je poursuis des études autonomes avec React, Python (en utilisant Streamlit et JupyterLab), et je participe aux programmes de cybersécurité et d'UX de Google.</h3>
             <div className='btn-groups flex'>
             <Link to="/portfolio" >
             <button type = "button" className='btn-item bg-brown fw-4 ls-2'>Voir mes projets</button>
